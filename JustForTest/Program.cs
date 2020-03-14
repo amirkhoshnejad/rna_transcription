2e5839149967173c1431e1937c0148509e21c55d
amirreza <45253403+amirkhoshnejad@users.noreply.github.com>
2020-03-15 00:07:41 +0330

    Add files via upload

---
+++ JustForTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace JustForTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "GTACGGT";  
+            char[] charArr = sentence.ToCharArray();  
+            char[] charArray= new char[charArr.Length]; 
+            for (int i = 0; i < charArr.Length; i++)
+            {
+                if (charArr[i] == 'G')
+                { 
+                    charArray[i]='C';
+                } 
+                if (charArr[i] == 'C')
+                {
+                    charArray[i]='G';
+                } 
+                if (charArr[i]== 'A')
+                {
+                    charArray[i]='T';
+                } 
+                if (charArr[i] == 'T')
+                {
+                    charArray[i]='A';
+                } 
+            }
+            string str = new string(charArray);
+            Console.WriteLine("///"+str);
+        }
+    }
+}
